ca08ae44053ada5e9ff86f0f11adc4d224301423
Sergio Valdez <sergioavr93@gmail.com>
2024-10-13 14:58:47 -0600

    Testing supabase

--- web/src/pages/IndexPage.vue
+++ web/src/pages/IndexPage.vue
@@ -1,47 +1,76 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
+    <!-- <example-component
       title="Example component"
       active
       :todos="todos"
       :meta="meta"
-    ></example-component>
+    ></example-component> -->
+    <pre>{{ txs }}</pre>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+import type { Ref } from 'vue'
+// import { Todo, Meta } from 'components/models';
+// import ExampleComponent from 'components/ExampleComponent.vue';
 
-defineOptions({
-  name: 'IndexPage'
-});
+import { createClient } from '@supabase/supabase-js'
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  '',
+  ''
+)
+console.log(supabase)
 
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+interface ITxs {
+  int: number,
+  date: string,
+  amount: number
+}
+
+const txs: Ref<ITxs[]> = ref([])
+
+/* const showTransaction = async () => {
+  console.info(supabase)
+  let { data: transactions, error } = await supabase
+    .from('transactions')
+    .select('id, date, amount')
+    console.error(error)
+  txs.value = transactions
+} */
+
+
+// defineOptions({
+//   name: 'IndexPage'
+// });
+
+// const todos = ref<Todo[]>([
+//   {
+//     id: 1,
+//     content: 'ct1'
+//   },
+//   {
+//     id: 2,
+//     content: 'ct2'
+//   },
+//   {
+//     id: 3,
+//     content: 'ct3'
+//   },
+//   {
+//     id: 4,
+//     content: 'ct4'
+//   },
+//   {
+//     id: 5,
+//     content: 'ct5'
+//   }
+// ]);
+
+// const meta = ref<Meta>({
+//   totalCount: 1200
+// });
 </script>
